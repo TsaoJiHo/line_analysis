0bfb2f3420afc9de78474eef5b3d049baefa0a4a
Gamania <hl654ck6a@gmail.com>
2020-06-27 03:59:51 +0800

    6/27

--- main.py
+++ main.py
@@ -37,24 +37,53 @@ def main():
         if month < 0:
             month += 12
             year -= 1
-        st.header(f'聊天總長 : {year} 年{month} 月{day} 天')
+        days = len(list(df.groupby('date').groups))
+        st.header(f'聊天持續 : {year} 年{month} 月{day} 天')
+        st.header(f'聊天天數 : {days} 天')
         st.header(f'訊息總數 : {len(df)}')
         # phone time
         # st.write(list(df['text']))
 
     # plot message count by name
-    d = {'name': [], 'msg count': []}
+    d = {'name': [], '訊息數': []}
     names = df.groupby('name').groups
     for name in names:
         d['name'].append(name)
-        d['msg count'].append(len(df[df['name'] == name]))
+        d['訊息數'].append(len(df[df['name'] == name]))
     plot_df = pd.DataFrame(d)
-    plot_df = plot_df.sort_values(by=['msg count'], ascending=False)
-    fig = px.bar(plot_df, y='msg count', x='name', text='msg count')
+    plot_df = plot_df.sort_values(by=['訊息數'], ascending=False)
+    fig = px.bar(plot_df, y='訊息數', x='name', text='訊息數')
     fig.update_layout(title_text='訊息總數分佈')
     st.write(fig)
     if len(names) > 2:
         st.write()
+
+    # plot message count by date
+    d = {'date': [], '訊息數': []}
+    dates = df.groupby('date').groups
+    for date in dates:
+        d['date'].append(date)
+        filter = df['date'] == date
+        d['訊息數'].append(len(df[filter]))
+    plot_df = pd.DataFrame(d)
+    fig = px.line(plot_df, x='date', y='訊息數')
+    fig.update_layout(title_text='每日訊息總數')
+    st.write(fig)
+
+    d = {'name':[], 'date': [], '訊息數': []}
+    dates = df.groupby('date').groups
+    for date in dates:
+        for name in names:
+            d['name'].append(name)
+            d['date'].append(date)
+            filter1 = df['date'] == date
+            filter2 = df['name'] == name
+            d['訊息數'].append(len(df[filter1][filter2]))
+    plot_df = pd.DataFrame(d)
+    if not plot_df.empty:
+        fig = px.line(plot_df, x='date', y='訊息數', color='name')
+        fig.update_layout(title_text='每日訊息總數')
+        st.write(fig)
 
     st.title('Row Data')
     st.write(df)
